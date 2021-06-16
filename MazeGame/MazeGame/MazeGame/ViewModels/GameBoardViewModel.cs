9000ceb96a5d4c0a74426b3d67297264a592d0ef
Sebastian Bobrowski <sebastian.bobrowski@outlook.com>
2021-06-16 18:03:09 +0200

    Added controls for the gameplay.

--- MazeGame/MazeGame/MazeGame/ViewModels/GameBoardViewModel.cs
+++ MazeGame/MazeGame/MazeGame/ViewModels/GameBoardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
 using MazeGame.Helpers;
 using MazeGame.Views;
 using Q_Learning;
@@ -24,36 +25,72 @@ namespace MazeGame.ViewModels
         private Grid GetContent()
         {
             var grid = new Grid();
-
-            grid.Children.Add(GetButton());
+            
             grid.Children.Add(new Maze
             {
                 BindingContext = _MazeViewModel
             });
+            
+            grid.Children.Add(GetControls());
 
             return grid;
         }
 
-        private Button GetButton()
+        private StackLayout GetControls()
         {
-            var button = new Button
+            var stack = new StackLayout()
             {
-                Text = "START",
-                MinimumHeightRequest = 50,
-                MinimumWidthRequest = 150,
+                Orientation = StackOrientation.Vertical,
                 HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Start
+                VerticalOptions = LayoutOptions.End,
+                Margin = new Thickness(0, 0, 0, 25)
             };
+            
+            stack.Children.Add(new Button()
+            {
+                Text = "Up",
+                WidthRequest = 50,
+                HeightRequest = 50,
+                Margin = new Thickness(50, 0, 50,0)
+            });
 
-            button.Clicked += async (sender, args) => _MazeViewModel.VisualizeWalk();
+            var internalStack = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                Spacing = 50
+            };
+            
+            stack.Children.Add(internalStack);
+            
+            internalStack.Children.Add(new Button()
+            {
+                Text = "Left",
+                WidthRequest = 50,
+                HeightRequest = 50,
+            });
+            
+            internalStack.Children.Add(new Button()
+            {
+                Text = "Right",
+                WidthRequest = 50,
+                HeightRequest = 50
+            });
+            
+            stack.Children.Add(new Button()
+            {
+                Text = "Down",
+                WidthRequest = 50,
+                HeightRequest = 50,
+                Margin = new Thickness(50, 0, 50,0)
+            });
 
-            return button;
+            return stack;
         }
-        
+
         private MazeSettings GetMazeSettings(MazeModel model) => new MazeSettings
         {
             XPos = Width / 2,
-            YPos = Height / 2,
+            YPos = (model.SizeOfCell * model.QuantityOfRows) / 2 + 15,
             WindowHeight = Height,
             WindowWidth = Width,
             Model = model
