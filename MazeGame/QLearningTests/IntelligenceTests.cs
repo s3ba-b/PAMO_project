3db782903ce039c04c6a6f04b07a68fea9dbcc52
Katarzyna Czerwińska <k.czerwinska2@gmail.com>
2021-06-19 13:28:14 +0200

    Added project for Qlearning unit tests

---
+++ MazeGame/QLearningTests/IntelligenceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Q_Learning;
+using Xunit;
+
+namespace QLearningTests
+{
+    public class IntelligenceTests
+    {
+        private Intelligence _sut;
+
+        public IntelligenceTests()
+        {
+            _sut = new Intelligence(new MazeModel());
+        }
+        
+        [Fact]
+        public void AreTestsWorking()
+        {
+            Assert.True(true);
+        }
+    }
+}
